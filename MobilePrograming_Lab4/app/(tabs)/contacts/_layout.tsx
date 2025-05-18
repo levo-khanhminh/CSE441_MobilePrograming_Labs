138dfd3fdd3a628c40f0ececae8b4a00bc8c3280
Le Vo Khanh Minh <148637435+khanhminh-se@users.noreply.github.com>
2025-05-18 23:09:15 +0700

    updated Lab4

---
+++ MobilePrograming_Lab4/app/(tabs)/contacts/_layout.tsx
@@ -0,0 +1,30 @@
+import { Stack } from "expo-router";
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const _layout = () => {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: true,
+          headerTitle: "Contacts",
+          statusBarHidden: true,
+        }}
+      />
+      <Stack.Screen
+        name="details"
+        options={{
+          headerShown: true,
+          headerTitle: "Card Profile",
+          statusBarHidden: true,
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default _layout;
+
+const styles = StyleSheet.create({});
